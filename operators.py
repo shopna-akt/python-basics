40af196efa26e45209b13f95831a2b5386170d2a
shopna-akt <shopna.akter.cse@ulab.edu.bd>
2026-09-24 13:11:22 +0600

    add operators

---
+++ operators.py
@@ -0,0 +1,22 @@
+# assignment operator
+a = 2
+b = 3
+c = 4
+a += 2
+b *= 3
+c **= 3
+print ("a:",a)
+print ("b:", b)
+print ("c", c)
+
+#logical operator
+val1 = True
+val2 = False
+a = 5
+b = 2
+
+print (not(val1>val2))
+print (not True)
+print (val1 and val2)
+print ((val1 or val2) and val1)
+print ((a>b) and (a==b) or (a<b))
