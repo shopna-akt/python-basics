0d4c836da69d8a45fdb67edd9df63203294bfa5d
shopna-akt <shopna.akter.cse@ulab.edu.bd>
2026-09-24 14:08:21 +0600

    expression and comments

---
+++ expressionandcomments.py
@@ -0,0 +1,39 @@
+# expression execution
+a = "2"
+b = 3
+c = "#"
+print ((a+c)*b)
+print (2*c*b)
+
+a = 2
+b = 3
+c = 4
+d = 3.5
+e = 7
+f = -1
+print ((a*b)+c)
+print (a * b)
+print (e/d)
+print (e//d)
+print (e//a)
+print (f//a)
+
+""""when denominator is negative output will be negative"""
+
+a = 5
+b = 2
+print (a%b)
+
+a = -5
+b = 2
+print (a%b)
+
+a = 5
+b = -2
+print (a%b)
+
+a = -5
+b = -2
+print (a%b)
+
+
