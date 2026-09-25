0bb32ea0c6e5ff45d2876ad799199d160ae82700
shopna-akt <shopna.akter.cse@ulab.edu.bd>
2026-09-25 18:59:52 +0600

    String

---
+++ string.py
@@ -0,0 +1,26 @@
+str1 = "I am Shopna Akter. \nI am an aspiring data scientist "
+print (str1)
+
+str1 = "I learn python"
+str2 = "I am from CSE department"
+print (str1 + str2)
+
+str1 = "Shopna"
+str2 = "Akter"
+str3 = "I do programming"
+print (len (str1))
+print (len (str1 + str2))
+print (len (str3))
+print (str1[4])
+print (str3[1])
+print (str2[2:4])
+print (str3[2:8])
+print (str1.endswith("na"))
+print (str1.endswith("er"))
+print (str3.capitalize())
+print (str3.replace("do","write"))
+print (str1.replace("S", "C"))
+print (str2.find("t"))
+print (str2.find("w"))
+print (str2.find("er"))
+print (str3.count("g"))
